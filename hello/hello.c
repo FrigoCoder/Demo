23b953224078346513205a2927a51474284e9c0d
Frigo <frigocoder@gmail.com>
2019-08-17 17:41:31 +0200

    Intel syntax assembly.

--- hello/hello.c
+++ hello/hello.c
@@ -1,26 +1,16 @@
 static void print(char *string)
 {
-    asm volatile("mov   $0x09, %%ah\n"
-                 "int   $0x21\n"
-                 : /* no output */
-                 : "d"(string)
-                 : "ah");
-}
-
-int wrong1(void)
-{
-    print("Wrong1!\n$");
-    return 0;
+    asm volatile(
+        ".intel_syntax\n"
+        "mov ah,0x09\n"
+        "int 0x21\n"
+        : /* no output */
+        : "d"(string)
+        : "ah");
 }
 
 int dosmain(void)
 {
     print("Hello, World!\n$");
-    return 0;
-}
-
-int wrong2(void)
-{
-    print("Wrong2!\n$");
     return 0;
 }
