f3f644fc3a0053d599584581480e00e33fe79abe
Frigo <frigocoder@gmail.com>
2019-08-17 15:37:45 +0200

    Section alignment was the key to strip those zeroes.

--- hello/compile.bat
+++ hello/compile.bat
@@ -1,5 +1,5 @@
 call clean.bat
 
 gcc hello.c -c -fdata-sections -ffreestanding -ffunction-sections -m32 -march=i386 -nostartfiles -nodefaultlibs -nostdinc -nostdlib -Os -s -std=gnu99 || exit /b
-ld hello.o -o hello.tmp --gc-sections --nmagic --script=script.ld || exit /b
-strip hello.tmp -o hello.com --output-target=binary || exit /b
+ld hello.o -o hello.tmp --file-alignment 0 --gc-sections --nmagic --section-alignment 0 --script=script.ld || exit /b
+strip hello.tmp -o hello.com --output-target=binary --strip-all || exit /b
