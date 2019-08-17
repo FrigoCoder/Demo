86074b409292263acc2117b3a094e32cc2e12dc2
Frigo <frigocoder@gmail.com>
2019-08-17 11:44:44 +0200

    Added initial working version.

---
+++ hello/compile.bat
@@ -0,0 +1,4 @@
+gcc hello.c -c -ffreestanding -m32 -march=i386 -nostdinc -nostdlib -Os -s -std=gnu99 || exit /b
+ld hello.o -o hello.tmp --nmagic --script=script.ld || exit /b
+objcopy -O binary hello.tmp hello.com || exit /b
+rem rm hello.tmp || exit /b
---
+++ hello/readme.txt
@@ -0,0 +1,2 @@
+https://nullprogram.com/blog/2014/12/09/
+https://www.youtube.com/watch?v=Y7vU5T6rKHE
