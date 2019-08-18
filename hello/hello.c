ffd212c67addd666ddb1e1f6a0e5c1d1f2be5f41
Frigo <frigocoder@gmail.com>
2019-08-18 19:04:48 +0200

    I can't fucking progress with this.

--- hello/hello.c
+++ hello/hello.c
@@ -1,4 +1,4 @@
-void print(char* string)
+void print(char *string)
 {
     asm volatile(
         ".intel_syntax\n"
@@ -9,67 +9,54 @@ void print(char* string)
         : "ah");
 }
 
-void vesa () {
+void vesa()
+{
     int mode = 0x129 | (1 << 14);
     asm volatile(
         ".intel_syntax\n"
         "mov ax, 0x4f02\n"
-//        "mov bx, _mode\n"
+        //        "mov bx, _mode\n"
         "int 0x10\n"
         :
         : "b"(mode)
         : "ax");
 }
 
-short current () {
+short current()
+{
     short result;
     asm volatile(
         ".intel_syntax\n"
         "mov ax, 0x4f03\n"
         "int 0x10\n"
-        : "=b" (result)
+        : "=b"(result)
         :
         : "ax");
     return result;
 }
 
-void writechar (char c) {
-    char* str = "x$";
-    str[0] = c;
-    print(str);
+void writechar(char c)
+{
+    asm volatile(
+        ".intel_syntax\n"
+        "mov ah, 0x02\n"
+        "int 0x21\n"
+        : /* no output */
+        : "dl"(c)
+        : "ax");
 }
 
-static void printnumber (int x){
-    writechar('0' + 0);
-    writechar('0' + 1);
-    writechar('0' + 2);
-    writechar('0' + 3);
-    writechar('0' + 4);
-    writechar('0' + 5);
-    writechar('0' + 6);
-    writechar('0' + 7);
-    writechar('0' + 8);
-    writechar('0' + 9);
-    return;
-}
-
-int factorial (int x){
-    if( x == 0 ){
-        return 1;
+void printnumber(int x)
+{
+    if (x >= 10)
+    {
+        printnumber(x / 10);
     }
-    return x * factorial(x - 1);
+    writechar('0' + (x % 10));
 }
 
-int dosmain(void) {
-    writechar('0' + 0);
-    writechar('0' + 1);
-    writechar('0' + 2);
-    writechar('0' + 3);
-    writechar('0' + 4);
-    writechar('0' + 5);
-    writechar('0' + 6);
-    writechar('0' + 7);
-    writechar('0' + 8);
-    writechar('0' + 9);
+int dosmain(void)
+{
+    printnumber(123456789);
     return 0;
 }
