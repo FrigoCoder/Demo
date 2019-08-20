4e772e65cd50b058970c0ebe5bc23a846276ea13
Frigo <frigocoder@gmail.com>
2019-08-20 22:17:42 +0200

    YES! IT FUCKING WORKS! I MANAGED TO COLOR A TRUE COLOR VESA VIDEO MODE!

--- star/star.asm
+++ star/star.asm
@@ -2,111 +2,93 @@ org 100h
 
 WIDTH EQU 640
 HEIGHT EQU 480
-MODE EQU 0x4112
 
 section .text 
 
-; switch to vesa display mode
+; switch to vesa
+mov bx, 0x121
+video:
 mov ax, 0x4f02
-mov bx, MODE
 int 0x10
+mov bl, 0x12
+cmp ah, bh
+je video
 
-; get vbe mode information
-mov ax, 0x4f01
-mov bx, MODE
-mov di, modeinfoblock
-int 0x10
+; init screen
+push 0xa000
+pop es
 
-; set up linear frame buffer
-mov edi, [.PhysBasePtr]
+; init time
+mov word [t], 0
 
 ; main loop
+main:
 
-mov word [y], 0
-loopy:
+     ; init bank
+     mov word [bank], 0
 
-mov word [x], 0
-loopx:
+     ; init pixel
+     xor di, di
 
-; main loop core
+     mov word [y], 0
+     loopy:
 
-mov ax, [x]
-stosb
-inc edi
+          mov word [x], 0
+          loopx:
 
-mov ax, [y]
-stosb
-inc edi
+               ; switch screenbank if needed
+               test di, di
+               jnz skip_bank_switch
+                    mov ax, 0x4f05
+                    xor bx, bx
+                    mov dx, [bank]
+                    int 10h
+                    inc word [bank]
+               skip_bank_switch:
 
-add ax, [x]
-stosb
-inc edi
+               ; do the stuff
+               mov al, [x]
+               add al, [t]
+               stosb
 
-; end of main loop
+               mov al, [y]
+               add al, [t]
+               stosb
 
-inc word [x]
-cmp word [x], WIDTH
-jl loopx
+               mov al, [x]
+               add al, [y]
+               add al, [t]
+               stosb
+               stosb
 
-inc word [y]
-cmp word [y], HEIGHT
-jl loopy
+               inc word [x]
+               cmp word [x], WIDTH
+               jl loopx
+
+          inc word [y]
+          cmp word [y], HEIGHT
+          jl loopy
+
+     ; increase time
+     inc word [t]
+
+     ; check keyboard
+     in al, 0x60
+     dec al
+     jnz main
 
 ; switch to text mode
 mov ax, 0x0003
 int 0x10
 
 ; exit
-int 20h
-
+ret
 
 section .data 
 
-
 section .bss 
 
+t resw 1
+bank resw 1
 x resw 1
 y resw 1
-
-modeinfoblock:
-
-     ; Mandatory information for all VBE revisions
-    .ModeAttributes      resw 1      ; mode attributes
-    .WinAAttributes      resb 1      ; window A attributes
-    .WinBAttributes      resb 1      ; window B attributes
-    .WinGranularity      resw 1      ; window granularity
-    .WinSize             resw 1      ; window size
-    .WinASegment         resw 1      ; window A start segment
-    .WinBSegment         resw 1      ; window B start segment
-    .WinFuncPtr          resd 1      ; pointer to window function
-    .BytesPerScanLine    resw 1      ; bytes per scan line
-
-     ; Mandatory information for VBE 1.2 and above
-    .XResolution         resw 1      ; horizontal resolution in pixels or chars
-    .YResolution         resw 1      ; vertical resolution in pixels or chars
-    .XCharSize           resb 1      ; character cell width in pixels
-    .YCharSize           resb 1      ; character cell height in pixels
-    .NumberOfPlanes      resb 1      ; number of memory planes
-    .BitsPerPixel        resb 1      ; bits per pixel
-    .NumberOfBanks       resb 1      ; number of banks
-    .MemoryModel         resb 1      ; memory model type
-    .BankSize            resb 1      ; bank size in KB
-    .NumberOfImagePages  resb 1      ; number of images
-    .Reserved            resb 1      ; reserved for page function
-
-     ; Direct Color fields (required for direct/6 and YUV/7 memory models)
-    .RedMaskSize         resb 1      ; size of direct color red mask in bits
-    .RedFieldPosition    resb 1      ; bit position of lsb of red mask
-    .GreenMaskSize       resb 1      ; size of direct color green mask in bits
-    .GreenFieldPosition  resb 1      ; bit position of lsb of green mask
-    .BlueMaskSize        resb 1      ; size of direct color blue mask in bits
-    .BlueFieldPosition   resb 1      ; bit position of lsb of blue mask
-    .RsvdMaskSize        resb 1      ; size of direct color reserved mask in bits
-    .RsvdFieldPosition   resb 1      ; bit position of lsb of reserved mask
-    .DirectColorModeInfo resb 1      ; direct color mode attributes
-
-     ; Mandatory information for VBE 2.0 and above
-    .PhysBasePtr         resd 1      ; physical address for flat frame buffer
-    .OffScreenMemOffset  resd 1      ; pointer to start of off screen memory
-    .OffScreenMemSize    resw 1      ; amount of off screen memory in 1k units
-    .Reserved2           resb 206
