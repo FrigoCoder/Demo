8278e832183400a3f293ad90fa7769c0e341584a
Sandor Orosz <Sandor.Orosz@ibm.com>
2019-08-24 15:44:37 +0200

    Cut 16 bytes by decrementing x and y instead of incrementing.

--- star/star.asm
+++ star/star.asm
@@ -20,84 +20,82 @@ mov word [t], 0
 ; main loop
 main:
 
-     ; init bank
-     mov word [bank], 0
+    ; init bank
+    mov word [bank], 0
 
-     ; init pixel
-     xor di, di
+    ; init pixel
+    xor di, di
 
-     mov word [y], 0
-     loopy:
+    mov word [y], HEIGHT
+    loopy:
 
-          mov word [x], 0
-          loopx:
+        mov word [x], WIDTH
+        loopx:
 
-               ; switch screenbank if needed
-               test di, di
-               jnz skip_bank_switch
-                    mov ax, 0x4f05
-                    xor bx, bx
-                    mov dx, [bank]
-                    int 10h
-                    inc word [bank]
-               skip_bank_switch:
+            ; switch screenbank if needed
+            test di, di
+            jnz skip_bank_switch
+            mov ax, 0x4f05
+            xor bx, bx
+            mov dx, [bank]
+            int 10h
+            inc word [bank]
+            skip_bank_switch:
 
-               ; get uv coordinates and direction
-               fild word [x]
-               fidiv word [width]
-               fstp dword [dir]
+            ; get uv coordinates and direction
+            fild word [x]
+            fidiv word [width]
+            fstp dword [dir]
 
-               fild word [y]
-               fidiv word [height]
-               fstp dword [dir+8]
+            fild word [y]
+            fidiv word [height]
+            fstp dword [dir+8]
 
-               fld1
-               fstp dword [dir+16]
+            fld1
+            fstp dword [dir+16]
 
 
 
-               ; do the stuff
-               
-               fild word [t]            ; t
-               fild word [x]            ; x t
-               fadd st0, st1            ; x+t t
-               fistp word [result]      ; t
-               mov ax, [result]
-               stosb
+            ; do the stuff
 
-               fild word [y]            ; y t
-               fadd st0, st1            ; y+t t
-               fistp word [result]      ; t
-               mov ax, [result]
-               stosb
+            fild word [t]            ; t
+            fild word [x]            ; x t
+            fadd st0, st1            ; x+t t
+            fistp word [result]      ; t
+            mov ax, [result]
+            stosb
 
-               fild word [x]            ; x t
-               fild word [y]            ; y x t
-               fadd st0, st1            ; x+y x t
-               fadd st0, st2            ; x+y+t x t
-               fistp word [result]      ; x t
-               mov ax, [result]
-               stosb
-               stosb
+            fild word [y]            ; y t
+            fadd st0, st1            ; y+t t
+            fistp word [result]      ; t
+            mov ax, [result]
+            stosb
 
-               fstp st0                 ; t
-               fstp st0                 ;
+            fild word [x]            ; x t
+            fild word [y]            ; y x t
+            fadd st0, st1            ; x+y x t
+            fadd st0, st2            ; x+y+t x t
+            fistp word [result]      ; x t
+            mov ax, [result]
+            stosb
+            stosb
 
-               inc word [x]
-               cmp word [x], WIDTH
-               jl loopx
+            fstp st0                 ; t
+            fstp st0                 ;
 
-          inc word [y]
-          cmp word [y], HEIGHT
-          jl loopy
+            dec word [x]
+            jnz loopx
 
-     ; increase time
-     inc word [t]
+        dec word [y]
+        jnz loopy
 
-     ; check keyboard
-     in al, 0x60
-     dec al
-     jnz main
+    ; increase time
+    inc word [t]
+
+    ; check keyboard
+    in al, 0x60
+    dec al
+    jnz main
 
 ; switch to text mode
 mov ax, 0x0003
