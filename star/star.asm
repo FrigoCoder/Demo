d2afcd60db31e977b82c7af8dd27b78f32ae93e5
Frigo <frigocoder@gmail.com>
2019-08-26 22:10:05 +0200

    Whatever.

--- star/star.asm
+++ star/star.asm
@@ -28,24 +28,9 @@ main:
     ; init pixel
     xor di, di
 
-    ; calculate camera based on time
-    ; cam.z stays -1.0
-    ; cam.y goes [-1;1] based on seconds [0;60] or time [0;3600] assuming 60Hz
-    ; cam.x goes [-2;2] based on seconds [0;60] or time [0;3600] assuming 60Hz
-    fild word [t]               ;   t
-    fidiv word [_1800]          ;   t/1800
-    fldz                        ;   0           t/1800
-    fld1                        ;   1           0           t/1800
-    fsub                        ;   -1          t/1800
-    fst qword [cam.z]           ;   -1          t/1800
-    fadd                        ;   t/1800-1
-    fst qword [cam.y]           ;   t/1800-1
-    fadd st0                    ;   t/900-2
-    fstp qword [cam.x]          ;   -
-
     ; get uv coordinates and direction for z
-    fld1                        ;   1
-    fstp qword [dir.z]          ;   -
+    fld qword [_0_1]            ;   0.1
+    fstp qword [d.z]            ;   -
 
     mov word [y], HEIGHT
     loopy:
@@ -54,8 +39,9 @@ main:
         fild word [y]           ;   y
         fidiv word [height]     ;   y/H
         fsub qword [_0_5]       ;   y/H-0.5
-        fdiv qword [ratio]      ;   (y/H-0.5)*H/W    
-        fstp qword [dir.y]      ;   -
+        fdiv qword [ratio]      ;   (y/H-0.5)*H/W
+        fmul qword [_0_1]       ;   (y/H-0.5)*H/W*0.1
+        fstp qword [d.y]        ;   -
 
         mov word [x], WIDTH
         loopx:
@@ -64,7 +50,8 @@ main:
             fild word [x]       ;   x
             fidiv word [width]  ;   x/W
             fsub qword [_0_5]   ;   x/W-0.5
-            fstp qword [dir.x]  ;   -
+            fmul qword [_0_1]   ;   (y/H-0.5)*0.1
+            fstp qword [d.x]    ;   -
 
             ; switch screenbank if needed
             test di, di
@@ -77,44 +64,51 @@ main:
 
             ; volumetric rendering
 
-            ; initialize v and s
-            fldz                ;   0
-            fst qword [v.x]     ;   0
-            fst qword [v.y]     ;   0
-            fst qword [v.z]     ;   0
-            fstp qword [s]      ;   -
+            ; calculate camera based on time
+            ; cam.z stays -1.0
+            ; cam.y goes [-1;1] based on seconds [0;60] or time [0;3600] assuming 60Hz
+            ; cam.x goes [-2;2] based on seconds [0;60] or time [0;3600] assuming 60Hz
+            fild word [t]               ;   t
+            fidiv word [_1800]          ;   t/1800
+            fldz                        ;   0           t/1800
+            fld1                        ;   1           0           t/1800
+            fsub                        ;   -1          t/1800
+            fst qword [p.z]             ;   -1          t/1800
+            fadd                        ;   t/1800-1
+            fst qword [p.y]             ;   t/1800-1
+            fadd st0                    ;   t/900-2
+            fstp qword [p.x]            ;   -
 
+            ; initialize v
+            fldz                        ;   0
+            fst qword [v.x]             ;   0
+            fst qword [v.y]             ;   0
+            fstp qword [v.z]            ;   -
+
+            ; init s
+            xor bx, bx
             loops:
 
                 ; p = cam+dir*s
-                
-                fld qword [s]       ;   s
+                fld qword [p.x]     ;   p.x
+                fadd qword [d.x]    ;   p.x+d.x
+                fstp qword [p.x]    ;   -
 
-                fld qword [dir.x]   ;   d.x         s
-                fmul st0, st1       ;   d.x*s       s
-                fadd qword [cam.x]  ;   c.x+d.x*s   s
-                fstp qword [p.x]    ;   s
+                fld qword [p.y]     ;   p.y
+                fadd qword [d.y]    ;   p.y+d.y
+                fstp qword [p.y]    ;   -
 
-                fld qword [dir.y]   ;   d.y         s
-                fmul st0, st1       ;   d.y*s       s
-                fadd qword [cam.y]  ;   c.y+d.y*s   s
-                fstp qword [p.y]    ;   s
-
-                fld qword [dir.z]   ;   d.z         s
-                fmul st0, st1       ;   d.z*s       s
-                fadd qword [cam.z]  ;   c.z+d.z*s   s
-                fstp qword [p.z]    ;   s
-                fstp st0            ;   -
+                fld qword [p.z]     ;   p.z
+                fadd qword [d.z]    ;   p.z+d.z
+                fstp qword [p.z]    ;   -
 
                 ; kaliset(p)
-
                 fldz                ;   0
                 fstp qword [a]      ;   -
 
+                call dotpp          ;   dot(p)
+
                 mov cx, ITERATIONS
-
-                call dotpp                      ;   dot(p)
-
                 kaliteration:
 
                     fld qword [p.x]             ;   p.x                 dot(p)
@@ -145,19 +139,41 @@ main:
                     dec cx
                     jnz kaliteration
 
-                fstp st0                        ;   -
-                fld qword [s]                   ;   s
-                fld1                            ;   1   s
-                fadd                            ;   s+1
-                fst qword [s]                   ;   s+1
-                fld qword [_4]                  ;   4   s+1
-                fcomp st1                       ;   s+1
-                fstsw ax                        ;
-                fstp st0                        ;
-                sahf                            ;
-                ja loops
+                fstp st0
 
-                
+                fld qword [v.x]                 ;   v.x
+                fadd qword [a]                  ;   v.x+a
+                fst qword [v.x]                 ;   v.x+a
+                fistp dword [r]                 ;   -
+
+                fld qword [v.y]                 ;   v.y
+                fadd qword [a]                  ;   v.y+a
+                fstp qword [v.y]                ;   v.z+a
+                fistp dword [g]                 ;   -
+
+                fld qword [v.z]                 ;   v.z
+                fadd qword [a]                  ;   v.z+a
+                fstp qword [v.z]                ;   v.z+a
+                fistp dword [b]                 ;   -
+
+                inc bx
+                cmp bx, 40
+                jb loops
+
+            ;   put pixel
+            
+            mov eax, [r]
+            shr eax, 16
+            stosb
+
+            mov eax, [g]
+            shr eax, 16
+            stosb
+
+            mov eax, [b]
+            shr eax, 16
+            stosb
+            stosb
 
             dec word [x]
             jnz loopx
@@ -198,6 +214,7 @@ height dw HEIGHT
 ratio dq 1.3333333333333333333333333333333
 formuparam dq 0.53
 
+_0_1  dq 0.1
 _0_5  dq 0.5
 _4    dq 4.0
 _1800 dw 1800
@@ -208,22 +225,20 @@ t resw 1
 x resw 1
 y resw 1
 
-cam.x resq 1
-cam.y resq 1
-cam.z resq 1
-
-dir.x resq 1
-dir.y resq 1
-dir.z resq 1
-
-v.x resq 1
-v.y resq 1
-v.z resq 1
+d.x resq 1
+d.y resq 1
+d.z resq 1
 
 p.x resq 1
 p.y resq 1
 p.z resq 1
 
-s resq 1
+v.x resq 1
+v.y resq 1
+v.z resq 1
+
 a resq 1
-pa resq 1
+
+r resd 1
+g resd 1
+b resd 1
