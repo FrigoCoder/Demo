2a75fad87fa0f97ca0bdeda7b02f862477fec4ed
Frigo <frigocoder@gmail.com>
2019-08-26 22:59:32 +0200

    Inlined things.

--- resources/modified star nest 7.glsl
+++ resources/modified star nest 7.glsl
@@ -1,19 +1,13 @@
-// Star Nest by Pablo Roman Andrioli
-
-// This content is under the MIT License.
-
-#define iterations 15
-#define formuparam 0.53
 
 #define smin 0.0
 #define smax 4.0
 #define step 0.1
 
 float kaliset(vec3 p){
-    float a=0.;
-    for (int i=0; i<iterations; i++) {
+    float a=0.0;
+    for (int i=0; i<15; i++) {
 	    float len=length(p);
-        p=abs(p)/(len*len)-formuparam; // the magic formula
+        p=abs(p)/(len*len)-0.53; // the magic formula
         a+=abs(length(p)-len); // absolute sum of average change
     }
     return a*a*a; // add contrast
@@ -22,17 +16,16 @@ float kaliset(vec3 p){
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // camera
-    vec3 cam=vec3(0.,0.,-1.0);
-    cam+=vec3(4,2,0)*(iTime/60.0-0.5);
+    vec3 cam=vec3(0,0,-1)+vec3(4,2,0)*(iTime/60.0-0.5);
     
-	// get coords and direction
-	vec2 uv=fragCoord.xy/iResolution.xy-.5;
-	uv.y*=iResolution.y/iResolution.x;
-	vec3 dir=vec3(uv,1.0)*step;
+	// direction
+	vec3 dir=vec3(fragCoord.x/iResolution.x-0.5,
+                  (fragCoord.y/iResolution.y-0.5)*iResolution.y/iResolution.x,
+                  1.0)*step;
 
 	// volumetric rendering
     vec3 p=cam;
-    vec3 v=vec3(0.);
+    vec3 v=vec3(0,0,0);
     float fade=1.0;
     for( float s=smin; s<=smax; s+=step){
         p+=dir;
@@ -40,5 +33,5 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
         v+=vec3(s,s*s,s*s*s*s)*a*fade;
         fade*=0.73;
 	}
-	fragColor = vec4(v*.0001,1.);	
+	fragColor.xyz = v*0.0001;
 }
