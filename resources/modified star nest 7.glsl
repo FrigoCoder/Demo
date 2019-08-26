9e5fdeb0a976134491442731ca7f8921e4e446a1
Frigo <frigocoder@gmail.com>
2019-08-26 23:03:28 +0200

    Simplified things even more.

--- resources/modified star nest 7.glsl
+++ resources/modified star nest 7.glsl
@@ -1,8 +1,4 @@
 
-#define smin 0.0
-#define smax 4.0
-#define step 0.1
-
 float kaliset(vec3 p){
     float a=0.0;
     for (int i=0; i<15; i++) {
@@ -21,15 +17,16 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	// direction
 	vec3 dir=vec3(fragCoord.x/iResolution.x-0.5,
                   (fragCoord.y/iResolution.y-0.5)*iResolution.y/iResolution.x,
-                  1.0)*step;
+                  1.0)*0.1;
 
 	// volumetric rendering
     vec3 p=cam;
     vec3 v=vec3(0,0,0);
     float fade=1.0;
-    for( float s=smin; s<=smax; s+=step){
+    for( int step=0; step<=40; step++ ){
         p+=dir;
         float a=kaliset(p);
+        float s=float(step)*0.1;
         v+=vec3(s,s*s,s*s*s*s)*a*fade;
         fade*=0.73;
 	}
