c7020120b23c178e4d233abb89e75c90f8c1b42c
Frigo <frigocoder@gmail.com>
2019-08-27 21:32:48 +0200

    I said use float instead of double.

--- star/star.asm
+++ star/star.asm
@@ -69,55 +69,55 @@ main:
             fldz                        ;   0           t/1800
             fld1                        ;   1           0           t/1800
             fsub                        ;   -1          t/1800
-            fst double [p.z]            ;   -1          t/1800
+            fst float [p.z]            ;   -1          t/1800
             fadd                        ;   t/1800-1
-            fst double [p.y]            ;   t/1800-1
+            fst float [p.y]            ;   t/1800-1
             fadd st0                    ;   t/900-2
-            fstp double [p.x]           ;   -
+            fstp float [p.x]           ;   -
 
             loops:
 
             ; p.x += (x/W-0.5)*0.1
-            fld double [p.x]
+            fld float [p.x]
             fild int16 [x]
             fidiv int16 [width]
-            fsub double [_0_5]
-            fmul double [_0_1]
+            fsub float [_0_5]
+            fmul float [_0_1]
             faddp st1, st0
-            fstp double [p.x]
+            fstp float [p.x]
 
             ; p.y += (y/H-0.5)*H/W*0.1
-            fld double [p.y]
+            fld float [p.y]
             fild int16 [y]
             fidiv int16 [height]
-            fsub double [_0_5]
+            fsub float [_0_5]
             fimul int16 [height]
             fidiv int16 [width]
-            fmul double [_0_1]
+            fmul float [_0_1]
             faddp st1, st0
-            fstp double [p.y]
+            fstp float [p.y]
 
             ; p.z += 0.1
-            fld double [p.z]
-            fadd double [_0_1]
-            fstp double [p.z]
+            fld float [p.z]
+            fadd float [_0_1]
+            fstp float [p.z]
 
             ; put pixel
-            fld double [p.x]
+            fld float [p.x]
             fimul int16 [_1800]
             fistp int32 [r]
             mov eax, [r]
             add al, 128
             stosb
 
-            fld double [p.y]
+            fld float [p.y]
             fimul int16 [_1800]
             fistp int32 [g]
             mov eax, [g]
             add al, 128
             stosb
 
-            fld double [p.z]
+            fld float [p.z]
             fimul int16 [_1800]
             fistp int32 [b]
             mov eax, [b]
@@ -150,12 +150,12 @@ section .data
 
 width def_int16 WIDTH
 height def_int16 HEIGHT
-ratio def_double 1.3333333333333333333333333333333
-formuparam def_double 0.53
+ratio def_float 1.3333333333333333333333333333333
+formuparam def_float 0.53
 
-_0_1  def_double 0.1
-_0_5  def_double 0.5
-_4    def_double 4.0
+_0_1  def_float 0.1
+_0_5  def_float 0.5
+_4    def_float 4.0
 _1800 def_int16 1800
 
 section .bss 
@@ -164,19 +164,19 @@ t res_int16 1
 x res_int16 1
 y res_int16 1
 
-d.x res_double 1
-d.y res_double 1
-d.z res_double 1
+d.x res_float 1
+d.y res_float 1
+d.z res_float 1
 
-p.x res_double 1
-p.y res_double 1
-p.z res_double 1
+p.x res_float 1
+p.y res_float 1
+p.z res_float 1
 
-v.x res_double 1
-v.y res_double 1
-v.z res_double 1
+v.x res_float 1
+v.y res_float 1
+v.z res_float 1
 
-a res_double 1
+a res_float 1
 
 r res_int32 1
 g res_int32 1
