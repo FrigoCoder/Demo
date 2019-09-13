58cfaa115ab113e63f043886aa14e813a10c17d0
Frigo <frigocoder@gmail.com>
2019-09-13 21:25:00 +0200

    Back to increasing time.

--- pastel/pastel.asm
+++ pastel/pastel.asm
@@ -21,6 +21,9 @@ ITERATIONS EQU 20
 
 section .text
 
+; init time
+mov int16 [frames], ax
+
 ; switch to vesa
 mov ax, 0x4f02
 mov bx, 0x10f
@@ -32,6 +35,9 @@ pop es
 
 ; main loop
 main:
+
+    ; progress time
+    inc int16 [frames]
 
     ; init bank
     xor dx, dx
@@ -188,8 +194,9 @@ main:
         cmp bx, HEIGHT/2
         jl loopy
 
-    ; decrease time
-    dec int16 [frames]
+    ; check keyboard
+    in al, 0x60
+    dec al
     jnz main
 
 ; switch to text mode
@@ -205,10 +212,10 @@ section .data
 width def_int16 WIDTH
 _255_per_iterations def_float 12.75
 _0_02 def_float 0.02
-frames def_int16 900
 fps def_int16 15
 
 
 section .bss
 
+frames res_int16 1
 u res_float 1
